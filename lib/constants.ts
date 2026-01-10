459fd3ce921dd9fbb112ce58740aab70bcac3c61
Mko1999 <mkrtich.okroyan@gmail.com>
2026-01-11 00:07:26 +0400

    header component

---
+++ lib/constants.ts
@@ -0,0 +1,14 @@
+export const NAV_ITEMS = [
+  {
+    title: 'Dashboard',
+    href: '/',
+  },
+  {
+    title: 'Search',
+    href: '/search',
+  },
+  {
+    title: 'Watchlist',
+    href: '/watchlist',
+  },
+];
