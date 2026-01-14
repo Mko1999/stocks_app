cd828ce65a920643f57b7720011f6452115c6710
Mko1999 <mkrtich.okroyan@gmail.com>
2026-01-14 23:54:15 +0400

    auth ui layout

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,83 @@
+'use client';
+
+import FooterLink from '@/components/forms/FooterLink';
+import InputField from '@/components/forms/InputField';
+import { Button } from '@/components/ui/button';
+import { useForm } from 'react-hook-form';
+
+const SignIn = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    control,
+    formState: { errors, isSubmitting },
+  } = useForm<SignInFormData>({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    mode: 'onBlur',
+  });
+
+  const onSubmit = async (data: SignInFormData) => {
+    try {
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div>
+      <h1 className="form-title">Log in to your account</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <InputField
+          name="email"
+          label="Email"
+          placeholder="john.doe@example.com"
+          register={register}
+          error={errors.email}
+          validation={{
+            required: 'Email is required',
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+            },
+            message: 'Email address is required',
+          }}
+        />
+
+        <InputField
+          type="password"
+          name="password"
+          label="Password"
+          placeholder="Password"
+          register={register}
+          error={errors.password}
+          validation={{
+            required: 'Password is required',
+            minLength: {
+              value: 8,
+              message: 'Password must be at least 8 characters',
+            },
+          }}
+        />
+
+        <Button
+          type="submit"
+          disabled={isSubmitting}
+          className="yellow-btn w-full mt-5"
+        >
+          {isSubmitting ? 'Creating Account' : 'Log in '}
+        </Button>
+
+        <FooterLink
+          text="Don’t have an account?"
+          linkText="Sign up"
+          href="/sign-up"
+        />
+      </form>
+    </div>
+  );
+};
+
+export default SignIn;
